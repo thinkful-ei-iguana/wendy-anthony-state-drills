4171b0b119e7280702991e3f355998d79b9bc94e
Wendy Bartos <wendyebartos@gmail.com>
2019-11-13 11:29:23 -0800

    timer drill

--- src/state-drills/Bomb.js
+++ src/state-drills/Bomb.js
@@ -3,31 +3,34 @@ import React from "react";
 class Bomb extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { count: new Date() };
+    this.state = { count: 0 };
   }
 
   componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 1000);
+    this.timerID = setInterval(() => {
+      let newCount = this.state.count + 1;
+      this.setState({ count: newCount });
+    }, 1000);
   }
+
   componentWillUnmount() {
     clearInterval(this.timerID);
   }
-  tick() {
-    this.setState({
-      count: new Date()
-    });
+
+  //using seperate helper method
+  renderCount() {
+    if (this.state.count % 2 === 0 && this.state.count < 8) {
+      return "Tick";
+    } else if (this.state.count % 2 !== 0 && this.state.count < 8) {
+      return "Tock";
+    } else if (this.state.count >= 8) {
+      this.componentWillUnmount();
+      return "BOOM!!!";
+    }
   }
-  // handleIncrement = () => {
-  //   this.setState({ count: this.state.count + 1 });
-  //   this.setInterval;
-  // };
+
   render() {
-    return (
-      <div>
-        <p>Time is {this.state.count.toLocaleTimeString()}ing.........</p>
-      </div>
-    );
+    return <div>{this.renderCount()}</div>;
   }
 }
-
 export default Bomb;
